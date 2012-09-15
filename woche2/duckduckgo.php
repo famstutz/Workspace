64bb272c01c2fb86854a84fae6092d38e69b8bcf
Florian Amstutz <florian@amstutz.nu>
2012-09-15 19:20:17 +0200

    added woche 2 übungen

---
+++ woche2/duckduckgo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="http://duckduckgo.com">
+            <label for="q">Search term</label>
+            <input type="text" name="q" />
+            <input type="submit" value="Search" />
+        </form>
+    </body>
+</html>
